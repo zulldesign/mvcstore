6f74f52e689239180dd7f020ae11942094f6f4a2
zulldesign <admin@zulldesign.ml>
2014-10-25 01:34:56 +0800

    l

--- mvcstore/mvcstore/Controllers/StoreController.cs
+++ mvcstore/mvcstore/Controllers/StoreController.cs
@@ -23,10 +23,10 @@ namespace mvcstore.Controllers
         //
         // GET: /Store/Browse?genre=Disco
 
-        public string Browse(string genre)
+        public ActionResult Browse(string genre)
         {
-            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
-            return message;
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
         }
 
         //
