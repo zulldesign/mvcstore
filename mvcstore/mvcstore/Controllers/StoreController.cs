8d05590cfaf635536797b05c6eb101adb0fbf262
zulldesign <admin@zulldesign.ml>
2014-10-25 00:16:00 +0800

    l

---
+++ mvcstore/mvcstore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcstore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello From Store.Index()";
+        }
+
+        //
+        // GET: /Store/Browse
+
+        public string Browse()
+        {
+            return "Hello From Store.Browse()";
+        }
+
+        //
+        // GET: /Store/Details
+
+        public string Details()
+        {
+            return "Hello From Store.Details()";
+        }
+
+    }
+}
