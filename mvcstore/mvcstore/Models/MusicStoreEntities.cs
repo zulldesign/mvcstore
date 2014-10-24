50e417df951b9cfca7adf6981cd9935e6a466ef0
zulldesign <admin@zulldesign.ml>
2014-10-25 02:27:21 +0800

    l

--- mvcstore/mvcstore/Models/MusicStoreEntities.cs
+++ mvcstore/mvcstore/Models/MusicStoreEntities.cs
@@ -5,10 +5,6 @@ namespace mvcstore.Models
     public class MusicStoreEntities : DbContext
     {
         public DbSet<Album> Albums { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Artist> Artists { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Genre> Genres { get; set; }        
     }
 }
