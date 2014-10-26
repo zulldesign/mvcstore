0f6e1ebc3369bbbcb6f136c4753df43545585db0
zulldesign <admin@zulldesign.ml>
2014-10-26 12:49:37 +0800

    l

--- mvcstore/mvcstore/Controllers/StoreController.cs
+++ mvcstore/mvcstore/Controllers/StoreController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using mvcstore.Models;
@@ -16,8 +19,32 @@ namespace mvcstore.Controllers
 
         public ActionResult Index()
         {
-            var genres = storeDB.Genres.ToList();
+            var genres = new List<Genre> 
+            { 
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+
             return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse?genre=Disco
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
         }
 
     }
