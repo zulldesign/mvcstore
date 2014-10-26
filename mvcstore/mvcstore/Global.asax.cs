2ae43bc382a13b7e20dff1a8bd2bcd866e300146
zulldesign <admin@zulldesign.ml>
2014-10-26 15:36:40 +0800

    l

--- mvcstore/mvcstore/Global.asax.cs
+++ mvcstore/mvcstore/Global.asax.cs
@@ -12,23 +12,6 @@ namespace mvcstore
 
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
             System.Data.Entity.Database.SetInitializer(new mvcstore.Models.SampleData());
