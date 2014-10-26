93c027011f08599b30f8fc49682a0c98e92437fe
zulldesign <admin@zulldesign.ml>
2014-10-26 15:33:51 +0800

    l

--- mvcstore/mvcstore/Global.asax.cs
+++ mvcstore/mvcstore/Global.asax.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace mvcstore
