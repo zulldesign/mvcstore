1971f7660b88c55522d7d963455157503646ea83
zulldesign <admin@zulldesign.ml>
2014-10-26 13:40:18 +0800

    l

--- mvcstore/mvcstore/Controllers/StoreController.cs
+++ mvcstore/mvcstore/Controllers/StoreController.cs
@@ -37,7 +37,8 @@ namespace mvcstore.Controllers
 
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
+
             return View(album);
         }
 
