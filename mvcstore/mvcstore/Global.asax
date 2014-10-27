4375616d82a17d1a702cfc3c2650bbfbbcaa9554
zulldesign <admin@zulldesign.ml>
2014-10-27 09:41:24 +0800

    l

---
+++ mvcstore/mvcstore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcstore.MvcApplication" Language="C#" %>
---
+++ mvcstore/mvcstore/Models/MusicStoreEntities.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace mvcstore.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }        
+    }
+}
