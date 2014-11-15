31f2c21f6584c59fbf0cd1174b7b53bdb365ce1c
zulldesign <admin@zulldesign.ml>
2014-11-15 16:09:47 +0800

    l

--- mvcstore/MvcMusicStore/Controllers/HomeController.cs
+++ mvcstore/MvcMusicStore/Controllers/HomeController.cs
@@ -1,34 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
-        MusicStoreEntities storeDB = new MusicStoreEntities();
-
         public ActionResult Index()
         {
-            // Get most popular albums
-            var albums = GetTopSellingAlbums(5);
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-            return View(albums);
+            return View();
         }
 
-        private List<Album> GetTopSellingAlbums(int count)
+        public ActionResult About()
         {
-            // Group the order details by album and return
-            // the albums with the highest count
-
-            return storeDB.Albums
-                .OrderByDescending(a => a.OrderDetails.Count())
-                .Take(count)
-                .ToList();
+            return View();
         }
     }
 }
